0c1a172dc3189cf429ce61e49e63a5604e8d8c90
GMItaliano <glm.goncalo.moreira@gmail.com>
2024-06-28 15:35:39 +0100

    add/rpi4 plat desc

--- src/platform/rpi4/inc/plat/platform.h
+++ src/platform/rpi4/inc/plat/platform.h
@@ -6,12 +6,6 @@
 #ifndef __PLAT_PLATFORM_H__
 #define __PLAT_PLATFORM_H__
 
-#define DATA_MEMORY_ACCESS          0x13
-#define L2D_CACHE_ACCESS            0x16
-#define L2D_CACHE_REFILL            0x17
-#define BUS_ACCESS                  0x19
-#define EXTERNAL_MEMORY_REQUEST     0xC0
-
 #define UART8250_REG_WIDTH   (4)
 #define UART8250_PAGE_OFFSET (0x40)
 
